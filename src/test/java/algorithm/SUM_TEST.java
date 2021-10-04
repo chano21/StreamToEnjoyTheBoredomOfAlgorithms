4ba413a2fbf8cdffd56b2a2dff1ab29899360e8b
chano <chano22@naver.com>
2021-10-04 17:39:33 +0900

    feat : 초기세팅

---
+++ src/test/java/algorithm/SUM_TEST.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SUM_TEST {
+
+	List<Integer> numberList=new ArrayList<Integer>();
+	
+	@BeforeEach
+	public void init() {
+		numberList=Arrays.asList(1,2,3,4,5,6,7);
+	}
+	
+	
+	@Test
+	@DisplayName("stream sum")
+	void sum() {			
+		
+	}
+	
+}
